ba6561a122ed6a5e24dcea1f63efca5bf1408f0f
anrui <ruinand@live.com>
2022-08-13 16:55:10 +0800

    add new comp

---
+++ src/component/menu.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="menu-demo">
+    <a-menu
+      :style="{ width: '200px', height: '100%' }"
+      :default-open-keys="['0']"
+      :default-selected-keys="['0_2']"
+      accordion=true
+      show-collapse-button
+    >
+      <a-sub-menu key="0">
+        <template #icon><icon-apps></icon-apps></template>
+        <template #title>Navigation 1</template>
+        <a-menu-item key="0_0">Menu 1</a-menu-item>
+        <a-menu-item key="0_1">Menu 2</a-menu-item>
+        <a-menu-item key="0_2">Menu 3</a-menu-item>
+        <a-menu-item key="0_3">Menu 4</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="1">
+        <template #icon><icon-bug></icon-bug></template>
+        <template #title>Navigation 2</template>
+        <a-menu-item key="1_0">Menu 1</a-menu-item>
+        <a-menu-item key="1_1">Menu 2</a-menu-item>
+        <a-menu-item key="1_2">Menu 3</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="2">
+        <template #icon><icon-bulb></icon-bulb></template>
+        <template #title>Navigation 3</template>
+        <a-menu-item key="2_0">Menu 1</a-menu-item>
+        <a-menu-item key="2_1">Menu 2</a-menu-item>
+        <a-sub-menu key="2_2" title="Navigation 4">
+          <a-menu-item key="2_2_0">Menu 1</a-menu-item>
+          <a-menu-item key="2_2_1">Menu 2</a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+    </a-menu>
+  </div>
+  <!-- <p>{{Log}}</p> -->
+</template>
+<script>
+import {
+  IconMenuFold,
+  IconMenuUnfold,
+  IconApps,
+  IconBug,
+  IconBulb,
+} from '@arco-design/web-vue/es/icon';
+
+export default {
+  components: {
+    IconMenuFold,
+    IconMenuUnfold,
+    IconApps,
+    IconBug,
+    IconBulb,
+  },
+  data(){
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped>
+.menu-demo {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 0px;
+  background-color: var(--color-neutral-2);
+}
+</style>
